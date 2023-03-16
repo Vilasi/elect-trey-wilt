c86ed21b2d1979e9b4fd4c6978f977b9eabc3ac6
Vilasi <VilasiCoding@gmail.com>
2023-03-15 22:02:09 -0400

    Website Final Draft Before Publication - Removed Comments, Changed Font Family, Add Donation Button

--- app.js
+++ app.js
@@ -70,41 +70,6 @@ function responsiveResizing(innerWidth) {
     educationArticleChildDivs[0].classList.add('me-5');
   }
 
-  //* Resize nav links
-  // if (innerWidth < '1500' && innerWidth > '1200') {
-  //   for (let item of navItems) {
-  //     item.classList.remove('me-5');
-  //     item.classList.remove('me-3');
-  //     item.classList.remove('me-2');
-  //     item.classList.remove('me-1');
-  //     item.classList.add('me-4');
-  //   }
-  // } else if (innerWidth < '1200' && innerWidth > '1150') {
-  //   for (let item of navItems) {
-  //     item.classList.remove('me-5');
-  //     item.classList.remove('me-4');
-  //     item.classList.remove('me-2');
-  //     item.classList.remove('me-1');
-  //     item.classList.add('me-3');
-  //   }
-  // } else if (innerWidth < '1150') {
-  //   for (let item of navItems) {
-  //     item.classList.remove('me-5');
-  //     item.classList.remove('me-4');
-  //     item.classList.remove('me-3');
-  //     item.classList.remove('me-1');
-  //     item.classList.add('me-2');
-  //   }
-  // } else {
-  //   for (let item of navItems) {
-  //     item.classList.remove('me-4');
-  //     item.classList.remove('me-3');
-  //     item.classList.remove('me-2');
-  //     item.classList.remove('me-1');
-  //     item.classList.add('me-5');
-  //   }
-  // }
-
   if (innerWidth < '800') {
     header.classList.add('flex-column');
     headerChildDivs[0].classList.add('border-right-none');
